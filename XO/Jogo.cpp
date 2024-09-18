45e7cc1ca82b12a45cdaec23b1e0118b327269c9
Artumos_Goc <63540372+ArtumosGOC@users.noreply.github.com>
2024-09-17 22:34:47 -0300

    Add Jogo class for XO game implementation

---
+++ XO/Jogo.cpp
@@ -0,0 +1,61 @@
+#include "Jogo.h"
+
+
+Jogo::Jogo() : jogador1('X'), jogador2('O'), jogadorAtual(1) {}
+
+void Jogo::iniciar() {
+	setlocale(LC_ALL, "Portuguese");
+    char jogar_novamente;
+    do {
+        tabuleiro.inicializar();
+        tabuleiro.desenhar();
+
+        int jogador_vencedor = 0;
+
+        for (int i = 0; i < 9; i++) {
+            if (jogadorAtual == 1) {
+                std::cout << "É a vez do jogador " << jogadorAtual << ". Escolha uma posição: ";
+                int posicao;
+                std::cin >> posicao;
+
+                if (posicao < 1 || posicao > 9) {
+                    std::cout << "Posição inválida! Tente outra posição!\n";
+                    i--;
+                    continue;
+                }
+
+                if (!tabuleiro.colocarMarcador(posicao, jogador1.getMarcador())) {
+                    std::cout << "Posição ocupada! Tente outra posição!\n";
+                    i--;
+                    continue;
+                }
+            }
+            else {
+                jogador2.jogadaMaquina(tabuleiro);
+            }
+
+            tabuleiro.desenhar();
+
+            jogador_vencedor = tabuleiro.verificarVencedor();
+
+            if (jogador_vencedor == 1) {
+                std::cout << "Jogador " << jogadorAtual << " venceu! Parabéns!\n";
+                break;
+            }
+
+            trocarJogador();
+        }
+
+        if (jogador_vencedor == 0) {
+            std::cout << "O jogo empatou!\n";
+        }
+
+        std::cout << "Deseja jogar novamente? (s/n): ";
+        std::cin >> jogar_novamente;
+
+    } while (jogar_novamente == 's' || jogar_novamente == 'S');
+}
+
+void Jogo::trocarJogador() {
+    jogadorAtual = (jogadorAtual == 1) ? 2 : 1;
+}
---
+++ XO/Jogo.h
@@ -0,0 +1,22 @@
+#ifndef JOGO_H
+#define JOGO_H
+
+#include "Tabuleiro.h"
+#include "Jogador.h"
+
+#include <iostream>.
+#include <locale.h>
+
+class Jogo {
+private:
+    Tabuleiro tabuleiro;
+    Jogador jogador1;
+    Jogador jogador2;
+    int jogadorAtual;
+public:
+    Jogo();
+    void iniciar();
+    void trocarJogador();
+};
+
+#endif // JOGO_H
