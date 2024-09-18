a06e8b4c365b5f6a382cd5276c827e27f4804055
Artumos_Goc <63540372+ArtumosGOC@users.noreply.github.com>
2024-09-17 22:45:54 -0300

    Refactor locale setting in Jogo::iniciar() and add support for non-Windows platforms

--- XO/Jogo.cpp
+++ XO/Jogo.cpp
@@ -6,9 +6,6 @@ Jogo::Jogo() : jogador1('X'), jogador2('O'), jogadorAtual(1) {}
 void Jogo::iniciar() {
     #ifdef _WIN32
 	setlocale(LC_ALL, "Portuguese");
-    #else
-	setlocale(LC_ALL, "pt_BR.UTF-8");
-    #endif
     char jogar_novamente;
     do {
         tabuleiro.inicializar();
@@ -58,6 +55,57 @@ void Jogo::iniciar() {
         std::cin >> jogar_novamente;
 
     } while (jogar_novamente == 's' || jogar_novamente == 'S');
+    #else
+	setlocale(LC_ALL, NULL);
+    do {
+        tabuleiro.inicializar();
+        tabuleiro.desenhar();
+
+        int jogador_vencedor = 0;
+
+        for (int i = 0; i < 9; i++) {
+            if (jogadorAtual == 1) {
+                std::cout << "e a vez do jogador " << jogadorAtual << ". Escolha uma posicao: ";
+                int posicao;
+                std::cin >> posicao;
+
+                if (posicao < 1 || posicao > 9) {
+                    std::cout << "Posicao invalida! Tente outra posicao!\n";
+                    i--;
+                    continue;
+                }
+
+                if (!tabuleiro.colocarMarcador(posicao, jogador1.getMarcador())) {
+                    std::cout << "Posicao ocupada! Tente outra posicao!\n";
+                    i--;
+                    continue;
+                }
+            }
+            else {
+                jogador2.jogadaMaquina(tabuleiro);
+            }
+
+            tabuleiro.desenhar();
+
+            jogador_vencedor = tabuleiro.verificarVencedor();
+
+            if (jogador_vencedor == 1) {
+                std::cout << "Jogador " << jogadorAtual << " venceu! Parabens!\n";
+                break;
+            }
+
+            trocarJogador();
+        }
+
+        if (jogador_vencedor == 0) {
+            std::cout << "O jogo empatou!\n";
+        }
+
+        std::cout << "Deseja jogar novamente? (s/n): ";
+        std::cin >> jogar_novamente;
+
+    } while (jogar_novamente == 's' || jogar_novamente == 'S');
+    #endif
 }
 
 void Jogo::trocarJogador() {
